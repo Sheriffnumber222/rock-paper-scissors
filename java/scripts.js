6c20b283b1c205fff28a40f2e444ac73ebf74d76
Brett Sheriff <Sheriff222@gmail.com>
2022-05-05 13:23:00 -0700

    Add computerPlay and playRound functions

--- java/scripts.js
+++ java/scripts.js
@@ -1,13 +1,54 @@
 /*ComputerPlay will randomly generate a number and assign a value (Rock, Paper, or Scissors) to their nextMove based off the number */
 function computerPlay () {
-    let randomNumber = Math.floor(math.random()*100);
-    let nextMove = ``;
+    let randomNumber = Math.floor(Math.random()*100);
     if (randomNumber <= 33) {
-        nextMove = `Rock`;
+        computerSelection = `rock`;
     } else if (randomNumber <= 66) {
-        nextMove = `Paper`;
+        computerSelection = `paper`;
     } else {
-        nextMove = `Scissors`;
+        computerSelection = `scissors`;
     }
-    nextMove return;
+    return computerSelection;
 };
+
+/*playRound will prompt the user to enter a valid move (case-insensitive) and store it in playerSelection, 
+    if the user input is invalid it will give an error and re-prompt the question.
+Once a valid response is inputed, it will execute computerPlay.
+It will compare playerSelection to computerSelection with two IF statements and output the proper string to declare winner/loser/draw*/
+
+function playRound(roundResult) {
+    let playerPick = prompt(`Rock? Paper? or Scissors?`);
+    let playerSelection = playerPick.toLowerCase();
+    let computerSelection = computerPlay();
+    if (playerSelection === `rock`) {
+        if (computerSelection === 'rock') {
+            roundResult = `Draw! Two Rocks`;
+        } else if (computerSelection === `paper`) {
+            roundResult = 'You Lose! Paper beats Rock'; 
+        } else if (computerSelection === 'scissors'){
+            roundResult = `You Win! Rock beats Scissors`;
+        }
+    } else if (playerSelection === `paper`) {
+        if (computerSelection === 'rock') {
+            roundResult = `You Win! Paper beats Rock`;
+        } else if (computerSelection === `paper`) {
+            roundResult = 'Draw! Two Papers'; 
+        } else if (computerSelection === 'scissors'){
+            roundResult = `You Lose! Scissors beats Paper`;
+        }
+    } else if (playerSelection === `scissors`) {
+        if (computerSelection === 'rock') {
+            roundResult = `You Lose! Rock beats Scissors`;
+        } else if (computerSelection === `paper`) {
+            roundResult = 'You Win! Scissors beats Paper'; 
+        } else if (computerSelection === 'scissors'){
+            roundResult = `Draw! Two Scissors`;
+        }
+    } else {
+        alert("invalid input");
+        return;
+    }
+    return roundResult;
+};
+
+alert(playRound());
