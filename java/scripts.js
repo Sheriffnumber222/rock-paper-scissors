8a6434bfdb8f757b6d702830384b305769d86951
Brett Sheriff <Sheriff222@gmail.com>
2022-05-04 18:56:41 -0700

    First attempt at computerPlay

--- java/scripts.js
+++ java/scripts.js
@@ -1,1 +1,13 @@
-console.log("hello, world");
+/*ComputerPlay will randomly generate a number and assign a value (Rock, Paper, or Scissors) to their nextMove based off the number */
+function computerPlay () {
+    let randomNumber = Math.floor(math.random()*100);
+    let nextMove = ``;
+    if (randomNumber <= 33) {
+        nextMove = `Rock`;
+    } else if (randomNumber <= 66) {
+        nextMove = `Paper`;
+    } else {
+        nextMove = `Scissors`;
+    }
+    nextMove return;
+};
