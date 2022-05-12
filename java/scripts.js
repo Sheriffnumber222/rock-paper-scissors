047ea82e0daef075f8e1bb58256f436d4c35a7ba
Brett Sheriff <Sheriff222@gmail.com>
2022-05-12 16:46:35 -0700

    Add dom manipulation to project

--- java/scripts.js
+++ java/scripts.js
@@ -1,3 +1,10 @@
+const roundResultsMessage = document.querySelector(`#round-results-message`);
+const playerScoreMessage = document.querySelector('#player-score');
+const computerScoreMessage = document.querySelector(`#computer-score`);
+const gameEndMessage = document.querySelector(`#game-end`);
+let playerScore = 0;
+let computerScore = 0;
+
 /*ComputerPlay will randomly generate a number and assign a value (Rock, Paper, or Scissors) to their nextMove based off the number */
 function computerPlay () {
     let randomNumber = Math.floor(Math.random()*100);
@@ -15,60 +22,75 @@ function computerPlay () {
     if the user input is invalid it will give an error and re-prompt the question.
 Once a valid response is inputed, it will execute computerPlay.
 It will compare playerSelection to computerSelection with two IF statements and output the proper string to declare winner/loser/draw*/
+function playRound(playerSelection) {
+    
+    /* Stop function and display the end screen once the game is over */
+    if (playerScore === 5) {
+        gameEndMessage.innerHTML = `Congrats! You win the game!!!`;
+        return;
+    } else if (computerScore === 5) {
+        gameEndMessage.innerHTML = `Sorry! You lose the game!!!`;
+        return;
+    }
 
-function playRound() {
-    let playerPick = prompt(`Rock? Paper? or Scissors?`);
-    let playerSelection = playerPick.toLowerCase();
     let computerSelection = computerPlay();
     if (playerSelection === `rock`) {
         if (computerSelection === 'rock') {
-            alert(`Draw! Two Rocks`);
-            roundResult = 0;
+            roundResultsMessage.innerHTML = `Draw! Two Rocks`;
         } else if (computerSelection === `paper`) {
-            alert(`You Lose! Paper beats Rock`);
-            roundResult = -1;
+            roundResultsMessage.innerHTML = `You Lose! Paper beats Rock`;
+            computerScore++;
         } else if (computerSelection === 'scissors'){
-            alert(`You Win! Rock beats Scissors`);
-            roundResult = 1;
+            roundResultsMessage.innerHTML = `You Win! Rock beats Scissors`;
+            playerScore++;
         }
     } else if (playerSelection === `paper`) {
         if (computerSelection === `rock`) {
-            alert(`You Win! Paper beats Rock`);
-            roundResult = 1;
+            roundResultsMessage.innerHTML = `You Win! Paper beats Rock`;
+            playerScore++;
         } else if (computerSelection === `paper`) {
-            alert(`Draw! Two Papers`); 
-            roundResult = 0;
+            roundResultsMessage.innerHTML = `Draw! Two Papers`; 
         } else if (computerSelection === 'scissors'){
-            alert(`You Lose! Scissors beats Paper`);
-            roundResult = -1;
+            roundResultsMessage.innerHTML = `You Lose! Scissors beats Paper`;
+            computerScore++;
         }
     } else if (playerSelection === `scissors`) {
         if (computerSelection === 'rock') {
-            alert(`You Lose! Rock beats Scissors`);
-            roundResult = -1;
+            roundResultsMessage.innerHTML = `You Lose! Rock beats Scissors`;
+            computerScore++;
         } else if (computerSelection === `paper`) {
-            alert(`You Win! Scissors beats Paper`); 
-            roundResult = 1;
+            roundResultsMessage.innerHTML = `You Win! Scissors beats Paper`; 
+            playerScore++;
         } else if (computerSelection === `scissors`){
-            alert(`Draw! Two Scissors`);
-            roundResult = 0;
+            roundResultsMessage.innerHTML = `Draw! Two Scissors`;
         }
-    } else {
-        alert(`invalid input`);
-        roundResult = 99;
-    }
-    return roundResult;
+    } 
+
+    playerScoreMessage.innerHTML = `Player: ${playerScore}`;
+    computerScoreMessage.innerHTML = `Computer: ${computerScore}`;
 };
+
+/*This event Listener will call the playRound() function after the player chooses their move via button.
+    This will also let that function know which choice the player made. */
+
+const playerPickBtns = document.querySelectorAll(`button`);
+playerPickBtns.forEach((button) => {
+    button.addEventListener(`click`, () => {
+        playRound(button.id.slice(0, -4)); //slice removes the "-btn" from the class to become compatible with playRound
+    });
+});
+
 
 /*game() will create a loop that lets the user play 5 rounds against the computer.
     If the user enteres a invalid input it will not count as a round.
     The game will keep score and report a winner or loser at the end.*/
-function game(totalScore = 0) {
+
+/*function game(totalScore = 0) {
     for (let i = 0; i < 5; i++) {
         playRound();
         if (roundResult === 99) {
             i--;
-            totalScore -= 99;
+            totalScore -= 99;~
         }
     totalScore += roundResult;
     console.log(totalScore);
@@ -80,6 +102,4 @@ function game(totalScore = 0) {
     } else {
         alert('GAME DRAW! BORING!!!');
     }
-} 
-
-game();
+} */
